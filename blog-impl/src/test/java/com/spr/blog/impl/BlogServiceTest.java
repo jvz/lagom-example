22c24d480f97582ddbf0933b96bec52938d96f3c
Matt Sicker <matt.sicker@spr.com>
2016-11-11 12:46:41 -0600

    Clean up BlogServiceTest

--- blog-impl/src/test/java/com/spr/blog/impl/BlogServiceTest.java
+++ blog-impl/src/test/java/com/spr/blog/impl/BlogServiceTest.java
@@ -5,13 +5,15 @@ import com.spr.blog.api.PostContent;
 
 import org.junit.Test;
 
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.defaultSetup;
 import static com.lightbend.lagom.javadsl.testkit.ServiceTest.withServer;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Integration tests for the blog microservice.
@@ -24,23 +26,58 @@ public class BlogServiceTest {
         withServer(defaultSetup().withCassandra(true), server -> {
             final BlogService service = server.client(BlogService.class);
 
-            assertFalse(service.getPost("arbitrary nonexistent id").invoke().toCompletableFuture().get(5, TimeUnit.SECONDS).isPresent());
+            unknownBlogIdShouldBeEmpty(service);
 
-            final String id = service.addPost().invoke(PostContent.builder().title("Test").author("Guy").body("Hello, world!").build()).toCompletableFuture().get(5, TimeUnit.SECONDS);
-            assertNotNull(id);
+            final String id = addPostAndGetId(service);
+            verifyPostIdHasContent(service, id, newPostContent());
 
-            final PostContent content = getBlogPost(service, id);
-            assertEquals(PostContent.builder().title("Test").author("Guy").body("Hello, world!").build(), content);
-
-            final PostContent updated = content.withBody("<h1>Hello, world!</h1>");
-            service.updatePost(id).invoke(updated).toCompletableFuture().get(5, TimeUnit.SECONDS);
-
-            final PostContent actual = getBlogPost(service, id);
-            assertEquals(PostContent.builder().title("Test").author("Guy").body("<h1>Hello, world!</h1>").build(), actual);
+            updatePostShouldUpdateContent(service, id, newPostContent().withBody("Test body"));
         });
     }
 
-    private static PostContent getBlogPost(final BlogService service, final String id) throws InterruptedException, java.util.concurrent.ExecutionException, java.util.concurrent.TimeoutException {
-        return service.getPost(id).invoke().toCompletableFuture().get(5, TimeUnit.SECONDS).orElseThrow(AssertionError::new);
+    private static void unknownBlogIdShouldBeEmpty(final BlogService service) throws Exception {
+        // when we look up an arbitrary unknown blog post
+        final Optional<PostContent> response =
+                getOrTimeout(service.getPost("unknown entity id").invoke());
+
+        // then the response should be empty
+        assertThat(response).isNotPresent();
+    }
+
+    private static String addPostAndGetId(final BlogService service) throws Exception {
+        // when we add a post
+        final String id = getOrTimeout(service.addPost().invoke(newPostContent()));
+
+        // then the id should be a UUID
+        assertThat(id).matches("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
+
+        return id;
+    }
+
+    private static void updatePostShouldUpdateContent(final BlogService service, final String id,
+                                                      final PostContent content) throws Exception {
+        // when we update a post
+        getOrTimeout(service.updatePost(id).invoke(content));
+
+        // then the contents should be updated
+        verifyPostIdHasContent(service, id, content);
+    }
+
+    private static void verifyPostIdHasContent(final BlogService service, final String id,
+                                               final PostContent content) throws Exception {
+        // when we look up the post ID
+        final Optional<PostContent> response = getOrTimeout(service.getPost(id).invoke());
+
+        // then the post content should match
+        assertThat(response).hasValue(content);
+    }
+
+    private static PostContent newPostContent() {
+        return new PostContent("Hello, world!", "This is a test blog entry", "msicker");
+    }
+
+    private static <T> T getOrTimeout(final CompletionStage<T> stage)
+            throws InterruptedException, ExecutionException, TimeoutException {
+        return stage.toCompletableFuture().get(5, TimeUnit.SECONDS);
     }
 }
